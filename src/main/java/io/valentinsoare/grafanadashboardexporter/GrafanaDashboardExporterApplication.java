fc707028a89bb8c762bf8cea3f8c5a3b90f9977c
valentinsoare <soarevalentinn@gmail.com>
2024-07-06 07:27:41 +0300

    init phase

---
+++ src/main/java/io/valentinsoare/grafanadashboardexporter/GrafanaDashboardExporterApplication.java
@@ -0,0 +1,11 @@
+package io.valentinsoare.grafanadashboardexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaDashboardExporterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrafanaDashboardExporterApplication.class, args);
+    }
+}
---
+++ src/test/java/io/valentinsoare/grafanadashboardexporter/GrafanaDashboardExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.valentinsoare.grafanadashboardexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaDashboardExporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
